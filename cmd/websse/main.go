1085549c93614ed94fe0dc4a51b46d88d7830260
Bill Nixon <bnixon67@gmail.com>
2023-12-30 15:49:14 -0600

    added SSE server and example

---
+++ cmd/websse/main.go
@@ -0,0 +1,140 @@
+// Copyright 2023 Bill Nixon. All rights reserved.
+// Use of this source code is governed by the license found in the LICENSE file.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"html/template"
+	"log"
+	"log/slog"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"path/filepath"
+
+	"github.com/bnixon67/webapp/assets"
+	"github.com/bnixon67/webapp/webapp"
+	"github.com/bnixon67/webapp/webhandler"
+	"github.com/bnixon67/webapp/weblog"
+	"github.com/bnixon67/webapp/webserver"
+	"github.com/bnixon67/webapp/websse"
+	"github.com/bnixon67/webapp/webutil"
+)
+
+const (
+	ExitUsage    = iota + 1 // ExitUsage indicates a usage error.
+	ExitConfig              // ExitConfig indicates a config error.
+	ExitLog                 // ExitLog indicates a log error.
+	ExitHandler             // ExitHandler indicates a handler error.
+	ExitServer              // ExitServer indicates a server error.
+	ExitTemplate            // ExitTemplate indicates a template error.
+)
+
+func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
+	// Check command line for config file.
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s [config file]\n", os.Args[0])
+		os.Exit(ExitUsage)
+	}
+
+	// Read config.
+	cfg, err := webapp.ConfigFromJSONFile(os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to get config:", err)
+		os.Exit(ExitConfig)
+	}
+
+	// Initialize logging.
+	err = weblog.Init(
+		weblog.WithFilename(cfg.Log.Filename),
+		weblog.WithType(cfg.Log.Type),
+		weblog.WithLevel(cfg.Log.Level),
+		weblog.WithSource(cfg.Log.WithSource),
+	)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error initializing logger:", err)
+		os.Exit(ExitLog)
+	}
+
+	// Get directory for assets, using a default if not specified in config.
+	if cfg.App.AssetsDir == "" {
+		cfg.App.AssetsDir = assets.AssetPath()
+	}
+	assetsDir := cfg.App.AssetsDir
+
+	// Show config in log.
+	slog.Info("using config", slog.Any("config", cfg))
+
+	// Define custom template functions.
+	funcMap := template.FuncMap{
+		"ToTimeZone": webutil.ToTimeZone,
+		"Join":       webutil.Join,
+	}
+
+	// Parse templates.
+	pattern := filepath.Join(assetsDir, "tmpl", "*.html")
+	tmpl, err := webutil.TemplatesWithFuncs(pattern, funcMap)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error initializing templates:", err)
+		os.Exit(ExitTemplate)
+	}
+
+	// Create the web app.
+	app, err := webapp.New(
+		webapp.WithName(cfg.App.Name), webapp.WithTemplate(tmpl))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error creating new handler:", err)
+		os.Exit(ExitHandler)
+	}
+
+	cssFile := filepath.Join(assetsDir, "css", "w3.css")
+	icoFile := filepath.Join(assetsDir, "ico", "webapp.ico")
+
+	// Create a new ServeMux to handle HTTP requests.
+	mux := http.NewServeMux()
+
+	// Add middleware to mux.
+	// Functions are executed in reverse, so last added is called first.
+	h := webhandler.LogRequest(mux)
+	h = webhandler.AddLogger(h)
+	h = webhandler.AddRequestID(h)
+
+	sseServer := websse.NewServer()
+
+	mux.HandleFunc("/", app.RootHandler)
+	mux.HandleFunc("/w3.css", webutil.ServeFileHandler(cssFile))
+	mux.HandleFunc("/favicon.ico", webutil.ServeFileHandler(icoFile))
+	mux.HandleFunc("/event", sseServer.EventStreamHandler)
+	mux.HandleFunc("/send", sseServer.SendHandler)
+
+	// Create the web server.
+	srv, err := webserver.New(
+		webserver.WithAddr(cfg.Server.Host+":"+cfg.Server.Port),
+		webserver.WithHandler(h),
+		webserver.WithTLS(cfg.Server.CertFile, cfg.Server.KeyFile),
+		webserver.WithWriteTimeout(0),
+	)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error creating server:", err)
+		os.Exit(ExitServer)
+	}
+
+	sseServer.Start()
+
+	// Create a new context.
+	ctx := context.Background()
+
+	// Start the web server.
+	err = srv.Start(ctx)
+	if err != nil {
+		slog.Error("error starting server", slog.Any("err", err))
+		fmt.Fprintln(os.Stderr, "Error starting server:", err)
+		os.Exit(ExitServer)
+	}
+}
---
+++ websse/sse.go
@@ -0,0 +1,278 @@
+// Copyright 2023 Bill Nixon. All rights reserved.
+// Use of this source code is governed by the license found in the LICENSE file.
+
+/*
+Package main provides an implementation of a Server-Sent Events (SSE) server.
+
+It defines a Server which encapsulates all the logic required to handle
+SSE connections, register clients for messages, and broadcast messages
+to registered clients. The server supports multiple event types and
+ensures thread-safe operations through the use of mutexes.
+
+The package is designed to be integrated into a larger web application,
+allowing for real-time broadcasting of messages to web clients via an
+event stream. This is facilitated via EventStreamHandler used by clients
+to listen to an event stream.
+
+The main components include:
+- Client: Represents an event stream client.
+- Message: Represents a message in the event stream.
+- Server: Manages broadcasting messages to clients.
+*/
+package websse
+
+import (
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/bnixon67/webapp/webhandler"
+	"github.com/bnixon67/webapp/webutil"
+)
+
+// Client represents event stream clients.
+type Client struct {
+	id      string       // id is a unique id for the client.
+	msgChan chan Message // msgChan is used to send messages to clients.
+}
+
+// Message represents a message in the event stream.
+type Message struct {
+	Event string // Event identifies the type (name) of event.
+	Data  string // Data is the data field for the message.
+	ID    string // ID is the event ID in the client.
+	Retry int    // Retry is the reconnection time in milliseconds.
+}
+
+// Server encapsulates the server-sent event logic.
+type Server struct {
+	// mu is an mutex to access to eventClients.
+	mu sync.Mutex
+
+	// eventClients contains all clients registered for an event.
+	eventClients map[string][]*Client
+
+	// broadcast is the channel to send an event.
+	broadcast chan Message
+}
+
+// writeHeaders writes the necessary SSE headers.
+func writeHeaders(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*") // TODO: optional
+}
+
+// EventStreamHandler handles event stream client connections.
+func (s *Server) EventStreamHandler(w http.ResponseWriter, r *http.Request) {
+	// Get logger with request info and function name.
+	logger := webhandler.GetRequestLoggerWithFunc(r)
+
+	// Check if the HTTP method is valid.
+	if !webutil.ValidMethod(w, r, http.MethodGet) {
+		logger.Error("invalid method")
+		return
+	}
+
+	// Get event from query parameters.
+	event := r.URL.Query().Get("event")
+
+	// Add to list of receivers for this event.
+	id := webhandler.RequestID(r.Context())
+	client := s.addClient(id, event)
+
+	// Write necessary HTTP headers for SSE.
+	writeHeaders(w)
+
+	// Process messages and handle client disconnects.
+	s.processMessages(event, client, w, r, logger)
+
+	logger.Info("client done", "client.id", client.id)
+}
+
+// processMessages waits for and sends messages and handles client disconnects.
+func (s *Server) processMessages(event string, client *Client, w http.ResponseWriter, r *http.Request, logger *slog.Logger) {
+	for {
+		select {
+
+		case msg, ok := <-client.msgChan: // Message received.
+			if !ok {
+				logger.Error("message channel closed",
+					"client.id", client.id,
+					"event", event,
+				)
+				return
+			}
+
+			err := s.writeMessage(w, msg)
+			if err != nil {
+				slog.Error("failed to write message",
+					"err", err,
+					"message", msg,
+				)
+				return
+			}
+
+		case <-r.Context().Done(): // Client disconnected.
+			logger.Info("client disconnected",
+				"client.id", client.id,
+				"event", event,
+			)
+			s.removeClient(event, client)
+			return
+
+		}
+	}
+}
+
+// addClient creates a new client and adds it to the event client list.
+func (s *Server) addClient(id, event string) *Client {
+	client := &Client{
+		id:      id,
+		msgChan: make(chan Message, 10), // buffered channel
+	}
+
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.eventClients[event] = append(s.eventClients[event], client)
+
+	slog.Info("added client", "client.id", client.id, "event", event)
+
+	return client
+}
+
+// removeClient removes a client from the event client list.
+func (s *Server) removeClient(event string, client *Client) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	clients := s.eventClients[event]
+	for i, c := range clients { // find the client in the list
+		if c == client {
+			// avoid memory leak
+			clients[i] = nil
+
+			// remove element
+			s.eventClients[event] = append(clients[:i],
+				clients[i+1:]...)
+			break
+		}
+	}
+
+	slog.Info("removed client", "client.id", client.id, "event", event)
+}
+
+// writeMessage writes a message to the event stream of the client.
+func (s *Server) writeMessage(w http.ResponseWriter, msg Message) error {
+	if len(msg.Event) > 0 {
+		_, err := fmt.Fprintf(w, "event: %s\n", msg.Event)
+		if err != nil {
+			return err
+		}
+	}
+
+	if len(msg.Data) > 0 {
+		// TODO: remove timestamp
+		_, err := fmt.Fprintf(w, "data: %s %s\n",
+			time.Now().Format("15:04:05"), msg.Data)
+		if err != nil {
+			return err
+		}
+	}
+
+	if len(msg.ID) > 0 {
+		_, err := fmt.Fprintf(w, "id: %s\n", msg.Data)
+		if err != nil {
+			return err
+		}
+	}
+
+	// TODO: add Retry
+
+	// Newline required per standard.
+	_, err := fmt.Fprint(w, "\n")
+	if err != nil {
+		return err
+	}
+
+	// Flush to avoid any buffering
+	f, ok := w.(http.Flusher)
+	if !ok {
+		return errors.New("streaming not supported")
+	}
+
+	f.Flush()
+
+	return nil
+}
+
+// SendHandler will publish any message requests.
+func (app *Server) SendHandler(w http.ResponseWriter, r *http.Request) {
+	// Get logger with request info and function name.
+	logger := webhandler.GetRequestLoggerWithFunc(r)
+
+	// Check if the HTTP method is valid.
+	if !webutil.ValidMethod(w, r, http.MethodPost) {
+		logger.Error("invalid method")
+		return
+	}
+
+	event := r.URL.Query().Get("event")
+	data := r.URL.Query().Get("data")
+	id := r.URL.Query().Get("id")
+	// TODO: retry := r.URL.Query().Get("retry")
+
+	// Publish the event.
+	app.Publish(Message{Event: event, Data: data, ID: id})
+}
+
+// listenAndBroadcast listens for messages to broadcast to clients.
+// This should be started as a go routine to handle message to the channels.
+func (s *Server) listenAndBroadcast() {
+	// Receive events and broadcast to clients.
+	for event := range s.broadcast {
+		s.broadcastToClients(event)
+	}
+}
+
+// broadcastToClients sends a message to registered clients.
+func (s *Server) broadcastToClients(msg Message) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	clients := s.eventClients[msg.Event]
+
+	slog.Info("start broadcast", "event", msg, "clients", len(clients))
+
+	for _, client := range clients {
+		slog.Info("sending", "client.id", client.id, "message", msg)
+		client.msgChan <- msg
+	}
+
+	slog.Info("end broadcast", "event", msg, "clients", len(clients))
+}
+
+// Publish sends a message to the broadcast channel.
+func (s *Server) Publish(msg Message) {
+	s.broadcast <- msg
+}
+
+// NewServer returns a new server to process server-side events.
+func NewServer() *Server {
+	s := &Server{
+		eventClients: make(map[string][]*Client),
+		broadcast:    make(chan Message),
+	}
+
+	return s
+}
+
+// Start runs the server in a goroutine.
+func (s *Server) Start() {
+	go s.listenAndBroadcast()
+}
