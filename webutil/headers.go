d5fa49314bfb4a525e9ed6d193a8f0d8a35f029f
Bill Nixon <bnixon67@gmail.com>
2024-05-12 12:38:47 -0500

    cleanup

--- webutil/headers.go
+++ webutil/headers.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Bill Nixon. All rights reserved.
+// Copyright 2024 Bill Nixon. All rights reserved.
 // Use of this source code is governed by the license found in the LICENSE file.
 
 // Package webutils provides utility functions.
@@ -8,13 +8,14 @@ import (
 	"net/http"
 )
 
-func addHeaders(w http.ResponseWriter, headers map[string]string) {
+// SetHeaders applies headers to the HTTP response.
+func SetHeaders(w http.ResponseWriter, headers map[string]string) {
 	for key, value := range headers {
-		w.Header().Add(key, value)
+		w.Header().Set(key, value)
 	}
 }
 
-// SetNoCacheHeaders instructs the client to not cache the response.
+// SetNoCacheHeaders instructs the client not to cache the response.
 func SetNoCacheHeaders(w http.ResponseWriter) {
 	headers := map[string]string{
 		"Cache-Control": "no-cache, no-store, must-revalidate",
@@ -22,25 +23,25 @@ func SetNoCacheHeaders(w http.ResponseWriter) {
 		"Expires":       "0",
 	}
 
-	addHeaders(w, headers)
+	SetHeaders(w, headers)
 }
 
-// SetContentType configures the 'Content-Type' and related headers.
+// SetContentType sets the 'Content-Type' and associated security headers.
 func SetContentType(w http.ResponseWriter, contentType string) {
 	headers := map[string]string{
 		"Content-Type":           contentType,
 		"X-Content-Type-Options": "nosniff",
 	}
 
-	addHeaders(w, headers)
+	SetHeaders(w, headers)
 }
 
-// SetContentTypeText sets headers for client to interpret response as plain text.
+// SetContentTypeText sets HTTP headers for plain text responses.
 func SetContentTypeText(w http.ResponseWriter) {
 	SetContentType(w, "text/plain;charset=utf-8")
 }
 
-// SetContentTypeHTML sets headers for client to interpret response as HTML.
+// SetContentTypeHTML sets HTTP headers for HTML responses.
 func SetContentTypeHTML(w http.ResponseWriter) {
 	SetContentType(w, "text/html;charset=utf-8")
 }
