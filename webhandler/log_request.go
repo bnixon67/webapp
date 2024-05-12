8433860164ff08da85ece7b84b381fdf7a57e550
Bill Nixon <bnixon67@gmail.com>
2024-05-12 16:34:21 -0500

    cleanup

--- webhandler/log_request.go
+++ webhandler/log_request.go
@@ -4,23 +4,16 @@
 package webhandler
 
 import (
-	"log/slog"
 	"net/http"
 )
 
-// LogRequest is a middleware function that logs the details of incoming HTTP requests.
-// It is designed to wrap around other HTTP handlers, adding logging functionality to them.
+// LogRequest is a middleware function that logs the details of incoming
+// HTTP requests. It enhances HTTP handlers by logging each request before
+// passing control to the next handler.
 func LogRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Retrieve a logger pre-configured with request information.
-		// GetRequestLogger is used instead of obtaining the logger directly from the context.
-		// This ensures compatibility if AddLogger middleware was not.
-		logger := NewRequestLogger(r).With(slog.String("func", "LogRequest"))
+		NewRequestLogger(r).Info("HTTP request received")
 
-		// Log the incoming request.
-		logger.Info("received")
-
-		// Call the next handler in the chain with the updated context.
 		next.ServeHTTP(w, r)
 	})
 }
